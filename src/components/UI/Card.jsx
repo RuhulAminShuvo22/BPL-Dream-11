8323f7a87f39e78e5118b563740c8cb7e92a3c1c
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-29 01:18:50 +0600

    some change happended for mobile divice

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FaUser } from "react-icons/fa";
+import { FaFlag } from "react-icons/fa";
+
+const Card = ({player}) => {
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-sm">
+                <figure>
+                <img
+                  src={player.playerPicture}
+                  alt="Player Picture" />
+                </figure>
+                <div className="card-body">
+                <h2 className="card-title">
+                     <FaUser/>
+                     {player.playerName}
+                </h2>
+            
+                <div className='flex justify-between gap-2 items-center'>
+                    <div className='flex justify-between gap-2'>
+                        <FaFlag/>
+                        <p>{player.playerCountry}</p>
+                    </div>
+            
+                    <button className='btn '>{player.playerType} </button>
+            
+                </div>
+            
+                <div className="divider"></div>
+            
+                <h2 className='font-bold'>Rating ({player.rating})</h2>
+            
+                <div className='flex justify-between gap-4 font-bold'>
+                    <p>{player.battingStyle}</p>
+                    <p className='text-right'>{player.bollingStyle}</p>
+                </div>
+            
+                <div className="card-actions justify-between items-center">
+            
+                    <p className='font-semibold'>Price:${player.price}</p>
+            
+                  <button className="btn ">Choose Player</button>
+                </div>
+              </div>
+                        </div>
+        </div>
+    );
+};
+
+export default Card;
